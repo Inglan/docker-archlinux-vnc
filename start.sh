258dc543b75e0650d55389314e2e3ff65a4b2b3b
Ingo Wolf <me@ingowolf.au>
2024-12-09 13:59:50 +1100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,6 @@ umask 0077                # use safe default permissions
 chmod go-rwx "$HOME/.vnc" # enforce safe permissions
 chmod +x "$HOME/.vnc/xstartup"
 
-# chromium
-sed -i 's/\/usr\/bin\/chromium/\/usr\/bin\/chromium --no-sandbox/g' /usr/share/applications/chromium.desktop
-
 # Start TigerVNC
 if [ ! -z $VNC_PASSWD ]; then
 	vncpasswd -f <<< "$VNC_PASSWD" > "$HOME/.vnc/passwd"
